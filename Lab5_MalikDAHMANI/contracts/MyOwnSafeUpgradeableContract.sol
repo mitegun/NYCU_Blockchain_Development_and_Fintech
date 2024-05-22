110881ae2d64d93a70f99d3408afc95072f91b83
Malik <103610312+mitegun@users.noreply.github.com>
2024-05-22 16:22:46 +0200

    Add files via upload

---
+++ Lab5_MalikDAHMANI/contracts/MyOwnSafeUpgradeableContract.sol
@@ -0,0 +1,31 @@
+// MyOwnSafeUpgradeable, this should be behind a proxy
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MyOwnSafeUpgradeableContract {
+
+  address public owner;
+  uint256 counter;
+  bool init;
+
+  modifier onlyOwner(){
+    require(msg.sender == owner, "!owner");
+    _;
+  }
+
+  function initialize(address _owner) external {
+    require(!init, "initialized");
+    owner = _owner;
+    init = true;
+  }
+
+  function withdraw(address token, uint256 amount) external onlyOwner {
+    IERC20(token).transfer(msg.sender, amount);
+  }
+
+  function count() external onlyOwner {
+    counter++;
+  }
+}
